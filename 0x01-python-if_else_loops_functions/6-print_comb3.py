5d9818ce5708b40d5ff2052c378f7b2c78228cd0
mbukeRepo <mbuke404@gmail.com>
2021-09-07 15:51:13 +0200

    some little adjustments

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end="")
+        print("{}{}".format(i, j), end=", ")
         if i * 10 + j == 89:
-            print()
+            print("{}{}".format(i, j))
             break
-        print(", ", end="")
