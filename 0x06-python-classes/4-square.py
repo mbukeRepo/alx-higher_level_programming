84fdd5b368831e4cd4d711f0bf7e0592e209ec06
mbukeRepo <mbuke404@gmail.com>
2021-09-28 23:46:42 +0200

    linted my code with pep8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,6 +4,7 @@
 class Square:
     def __init__(self, size):
         self.size = size
+
     @property
     def size(self):
         return self.__size
@@ -15,5 +16,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size * self.__size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,9 +2,10 @@
 
 
 class Square:
-    
+
     def __init__(self, size):
         self.size = size
+
     @property
     def size(self):
         return self.__size
