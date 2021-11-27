41ef93481c82144e61d88f9b2418902a80a8b4fb
mbukeRepo <mbuke404@gmail.com>
2021-11-27 14:02:38 +0200

    update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,43 @@ class Base:
             else:
                 list_of_obj_dict = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_of_obj_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns an array of objects from json_string
+        Args:
+           json_string (str): json string representing array of objects
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Returns an instance from dictionary as its attributes
+        Args:
+           **dictionary (dict): key/value pairs to be used as attributes
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1,1)
+            else:
+                new = cls(1)
+
+            new.update(**dictionary)
+
+            return new
+        
+        @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances from file
+        """
+        filename = cls.__name__ + ".json"
+        
+        try:
+            with open(filename) as f:
+                list_dicts = Base.from_json_string(f.read())
+                return [cls.create(**obj) for obj in list_dicts]
+        except IOError:
+            return []
+        
