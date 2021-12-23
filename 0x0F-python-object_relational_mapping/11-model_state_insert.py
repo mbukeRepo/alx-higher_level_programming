9d278e174b68fe30b82f5af63be7a3e2451c224f
mbukeRepo <mbuke404@gmail.com>
2021-12-23 22:50:26 +0200

    solved 11 and 12

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# adds new state in the database
+# adds new row in states table
+# Usage: ./11-model_state_insert.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
+#                                   <state name searched>
+
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# updates the row with id = 2
+# Usage: ./12-model_state_update_id_2.py <mysql username> /
+#                                        <mysql password> /
+#                                        <database name>
+
+import sys
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
+    session.commit()
