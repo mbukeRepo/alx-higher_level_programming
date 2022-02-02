786653a3cc64db8ef429e2e3814f451e84177bfd
mbukeRepo <mbuke404@gmail.com>
2022-02-02 23:42:06 +0200

    reconfigured 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,9 +2,5 @@
 
 const fs = require('fs');
 const request = require('request');
-const options = { encoded: 'utf-8' };
-request(process.argv[2], (err, res, body) => {
-  if (!err) {
-    fs.writeFile(process.argv[3], body, options);
-  }
-});
+
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
